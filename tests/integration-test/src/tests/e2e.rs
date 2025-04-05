d8be4f638c69ba61ae6397d8cffa381f9b2f311a
Andrew McDermott <amcdermo@redhat.com>
2025-04-05 07:07:05 +0100

    snap

--- tests/integration-test/src/tests/e2e.rs
+++ tests/integration-test/src/tests/e2e.rs
@@ -1,12 +1,15 @@
 use std::{collections::HashMap, fs, path::PathBuf, thread::sleep, time::Duration};
 
 use bpfman::{
-    load_ebpf_programs, program_loader::LoadSpecBuilder, remove_program, setup, setup_with_sqlite, types::{AttachInfo, BytecodeImage, ImagePullPolicy, Location, TcProceedOn, XdpProceedOn}, ProgramType
+    ProgramType, load_ebpf_programs,
+    program_loader::LoadSpecBuilder,
+    remove_program, setup, setup_with_sqlite,
+    types::{AttachInfo, BytecodeImage, ImagePullPolicy, Location, TcProceedOn, XdpProceedOn},
 };
 use procfs::sys::kernel::Version;
 
 use crate::tests::{
-    basic::trigger_bpf_program, utils::*, RTDIR_FS_TC_EGRESS, RTDIR_FS_TC_INGRESS, RTDIR_FS_XDP,
+    RTDIR_FS_TC_EGRESS, RTDIR_FS_TC_INGRESS, RTDIR_FS_XDP, basic::trigger_bpf_program, utils::*,
 };
 
 pub(crate) const GLOBAL_U8: &str = "GLOBAL_u8";
@@ -1082,70 +1085,70 @@ fn test_load_unload_xdp_maps() {
 fn sqlite_test_load_unload_xdp_maps() {
     init_logger();
     let (_config, mut conn) = setup_with_sqlite().unwrap();
-    let _namespace_guard = create_namespace().unwrap();
-    let _ping_guard = start_ping().unwrap();
+    // let _namespace_guard = create_namespace().unwrap();
+    // let _ping_guard = start_ping().unwrap();
 
-    assert!(iface_exists(DEFAULT_BPFMAN_IFACE));
+    // assert!(iface_exists(DEFAULT_BPFMAN_IFACE));
 
-    println!("Installing xdp_counter program");
+    // println!("Installing xdp_counter program");
 
-    let source = Location::Image(BytecodeImage {
-        image_url: XDP_COUNTER_IMAGE_LOC.to_string(),
-        image_pull_policy: ImagePullPolicy::Always,
-        username: None,
-        password: None,
-    });
+    // let source = Location::Image(BytecodeImage {
+    //     image_url: XDP_COUNTER_IMAGE_LOC.to_string(),
+    //     image_pull_policy: ImagePullPolicy::Always,
+    //     username: None,
+    //     password: None,
+    // });
 
-    source.get_program_bytes()
+    // source.get_program_bytes()
 
-    let bytecode = fs::read(&*XDP_COUNTER_IMAGE_LOC).unwrap();
+    // let bytecode = fs::read(&*XDP_COUNTER_IMAGE_LOC).unwrap();
 
-    let globals = vec![
-        ("xdp".to_string(), b"foo".to_vec()),
-        ("another".to_string(), vec![1, 2, 3]),
-    ];
-
-    let load_spec = LoadSpecBuilder::default()
-        .bytecode_source(source)
-        .global_data(globals)
-        .program_bytes(bytecode)
-        // .programs(args.get_programs().to_vec())
-        .build().
-        unwrap();
+    // let globals = vec![
+    //     ("xdp".to_string(), b"foo".to_vec()),
+    //     ("another".to_string(), vec![1, 2, 3]),
+    // ];
 
     // let load_spec = LoadSpecBuilder::default()
     //     .bytecode_source(source)
-    //     .function_names(function_names)
-    //     .global_data(args.get_global_data().unwrap_or_default())
-    //     .map_owner_id(args.get_map_owner_id())
-    //     .metadata(args.get_metadata().unwrap_or_default())
-    //     .program_bytes(program_bytes)
-    //     .programs(args.get_programs().to_vec())
+    //     .global_data(globals)
+    //     .program_bytes(bytecode)
+    //     // .programs(args.get_programs().to_vec())
     //     .build().
-    //     unwrap()
+    //     unwrap();
 
-    let loaded = load_ebpf_programs(&mut conn, &load_spec).unwrap();
+    // // let load_spec = LoadSpecBuilder::default()
+    // //     .bytecode_source(source)
+    // //     .function_names(function_names)
+    // //     .global_data(args.get_global_data().unwrap_or_default())
+    // //     .map_owner_id(args.get_map_owner_id())
+    // //     .metadata(args.get_metadata().unwrap_or_default())
+    // //     .program_bytes(program_bytes)
+    // //     .programs(args.get_programs().to_vec())
+    // //     .build().
+    // //     unwrap()
 
-    assert_eq!(loaded[0].kind, ProgramType::Xdp);
+    // let loaded = load_ebpf_programs(&mut conn, &load_spec).unwrap();
 
-    let program = &loaded[0].program;
-    assert_eq!(program.kind, "xdp");
-    assert_eq!(program.state, "loaded");
-    assert!(program.map_pin_path.starts_with("/sys/fs/bpf/"));
+    // assert_eq!(loaded[0].kind, ProgramType::Xdp);
 
-    assert!(bpffs_has_entries(RTDIR_FS_XDP));
+    // let program = &loaded[0].program;
+    // assert_eq!(program.kind, "xdp");
+    // assert_eq!(program.state, "loaded");
+    // assert!(program.map_pin_path.starts_with("/sys/fs/bpf/"));
 
-    // let map_pin_path = loaded[0].maps[0].pin_path.as_ref().unwrap();
-    // assert!(map_pin_path.join("xdp_stats_map").exists());
+    // assert!(bpffs_has_entries(RTDIR_FS_XDP));
 
-    let map = &loaded[0].maps[0];
-    assert_eq!(map.name, "xdp_stats_map");
-    assert_eq!(map.max_entries.get(), 128);
+    // // let map_pin_path = loaded[0].maps[0].pin_path.as_ref().unwrap();
+    // // assert!(map_pin_path.join("xdp_stats_map").exists());
 
-    //unload_programs(&loaded).unwrap();
+    // let map = &loaded[0].maps[0];
+    // assert_eq!(map.name, "xdp_stats_map");
+    // assert_eq!(map.max_entries.get(), 128);
 
-    // Verify bpffs entries are cleaned up
-    assert!(!bpffs_has_entries(RTDIR_FS_XDP));
+    // //unload_programs(&loaded).unwrap();
+
+    // // Verify bpffs entries are cleaned up
+    // assert!(!bpffs_has_entries(RTDIR_FS_XDP));
 }
 
 #[test]
